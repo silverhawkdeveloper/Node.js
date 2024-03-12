eeaf9b2794234be221ed527861bcd531d5e1b793
Daniel <silverhawkdeveloper@gmail.com>
2024-03-12 16:39:26 +0100

    actualizar

--- basicos/http_call/http/app.js
+++ basicos/http_call/http/app.js
@@ -1,10 +1,11 @@
+
 const https = require('https');
 
-let username = 'silverhawkdeveloper';
+const username = 'silverhawkdeveloper';
 
-let CHROME_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
-let FIREFOX_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1';
-let IE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko';
+const CHROME_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+const FIREFOX_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1';
+const IE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko';
 
 let options = {
     host: 'api.github.com',
@@ -28,3 +29,5 @@ let request = https.request(options, (response) => {
 request.on('error', (e) => {
     console.log(e);
 });
+
+request.end();
--- basicos/node-fetch/app.js
+++ basicos/node-fetch/app.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 
-fetch('https://api.github.com/users/silverhawkdeveloper').then((respuesta) => {
+let promesa = fetch('https://api.github.com/users/silverhawkdeveloper');
+
+promesa.then((respuesta) => {
     return respuesta.json();
 }).then((json) => {
     console.log(json);
